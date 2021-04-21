f9d2797ff4d038f34280b52c0d830e8b1a0187c1
Andrea Esposito <andexpox@gmail.com>
2021-04-21 17:31:55 +0200

    add-new-todo

--- js/script.js
+++ js/script.js
@@ -3,3 +3,24 @@
 // - stampare in pagina un item per ogni elemento contenuto in un array
 // - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
 // - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        utenteNuovoTodo: '',
+        todoArray: [
+            'Fare i compiti',
+            'Fare la spesa',
+            'Fare il bucato'
+        ],
+    },
+    methods: {
+        
+        nuovoTodo() {
+            if (this.utenteNuovoTodo.length > 0) {
+                this.todoArray.push(this.utenteNuovoTodo);
+                this.utenteNuovoTodo = '';
+            }
+        }
+    }
+});
